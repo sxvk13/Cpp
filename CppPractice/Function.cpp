a9e0648cc6db3632e5e04cf2735f532474908aa4
김경식 <0345tlr@naver.com>
2022-03-05 21:20:47 +0900

    Lecture 33 ( Function )

--- CppPractice/Function.cpp
+++ CppPractice/Function.cpp
@@ -2,6 +2,14 @@
 
 using namespace std;
 
+
+//코드블럭 외부에 변수를 선언하게 되면 전역변수가 된다. 전역변수는 이 변수를
+//선언한 아래의 모든 부분에서 공유해서 사용이 가능하다.
+//전역변수는 소속된 코드블럭이 없으므로 프로그램이 종료될때까지 메모리에 존재하게 되고,
+//프로그램이 종료될 때 메모리에서 해제가 된다.
+
+int g_iNum = 123;
+
 // 함수 : 기능을 만들어 줌.
 // 일종의 사용할 수 있는 부품을 만들어 준다고 볼 수 있다.
 // 특정 상황에 동작하는 코드 집합을 만들어 내는 것이다.
@@ -14,7 +22,7 @@ using namespace std;
 // 인자 : 해당 함수를 사용할 때 넘겨줄 값이 있을경우 인자를 활용한다. 인자는 있을수도 없을수도 있다.
 
 // 해당 함수를 호출하면 코드블럭 안의 코드가 동작이 된다.
-
+// 아래 함수처럼 결과를 반환해주는 함수 형태를 call by value 라고하낟.
 int Sum(int a, int b) {
 	
 
@@ -29,14 +37,50 @@ int Sum(int a, int b) {
 void OutputText() {
 	cout << "OutputText Function!" << endl;
  }
+
+//아래 함수처러 인자를 넘겨받게 될 경우 이 인자는 이 함수 코드블럭의 지역에
+//속해있다고 판단한다. 즉, 이 함수의 지역변수가 되고 stack에 할당된다.
+//할당된 지역변수는 해당 지역변수가 소속된 코드블럭이 닫히게 되면 메모리에서 해제된다.
+
+
 void OutputNumber(int iNumber) {
 	cout << "Number : " << iNumber << endl;
 }
 
+
 void ChangeNumber(int iNumber) {
 	iNumber = 9999;
+	g_iNum = 456;
 }
 
+// 함수의 디폴트인자 : 인자는 기본적으로 값을 넘겨받게 설계되어 있다. 그런디 인자에 
+//	기본값을 대입해두면 인자를 넘겨주지 않을 경우 기본 설정한 값이 되고 인자를 넘겨주는
+//	경우 넘겨받은 값으로 인자를 설정하는 기능이다.
+// 단, 디폴트인자는 가장 오른쪽부터 지정해 주어야한다.
+
+void Output(int iNum1, int iNum2 = 10) {
+	
+	cout << iNum1 << endl;
+	cout << iNum2 << endl;
+}
+// 위의 함수는 main()함수에서 Output(10); 을 통해서 10과 10을 호출할 수 있지만,
+//void Output(int iNum1) {
+//	...
+//}
+//와 같이 인자가 한 개만 존재하는 오버로딩된 함수가 존재하면 Output(10)을 통해서 어떤 함수를
+//호출해야 할 지 모르기 때문에 주의해야한다.
+
+// 함수의 오버로딩 : 같은 이름의 함수이지만 기능이 다르던가 인자가 다른 함수를 말함.
+// 오버로딩의 조건 : 함수 이름이 무조건 같아야 한다. 단, 인자의 개수 혹은 타입이 달라야
+//					오버로딩이 성립됨. ( 반환 타입은 오버로딩에 영향을 주지 않는다)
+//
+
+
+// 아래 함수는 결과값을 인자로 받아와서 결과를 반환해 주었다. 이런 형태의 함수를
+// Call by Address 라고 한다.
+void ChangeNumber(int* pNumber) {
+	*pNumber = 2939;
+}
 
 
 int main() {
@@ -49,7 +93,21 @@ int main() {
 
 	OutputText();
 	OutputNumber(1234);
-	
+
+
+	// 아래처럼 특정 코드블럭 안에서 선언되는 변수를 지역변수라고 한다.
+	// 지역변수는 메모리 영역중 stack에 할당됨.
+
+	int iNumber = 0;
+	ChangeNumber(iNumber);
+	ChangeNumber(&iNumber);
+
+	cout << iNumber << endl;
+	cout << g_iNum << endl;
+
+	Output(10);
+
+
 
 	return 0;
 
